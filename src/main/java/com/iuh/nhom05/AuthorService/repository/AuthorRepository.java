75c0d4106ad2ace3edbbbec06c2b19ae76e9e213
catluynh <catluynh99@gmail.com>
2021-11-30 22:05:47 +0700

    reset project

--- src/main/java/com/iuh/nhom05/AuthorService/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iuh.nhom05.AuthorService.repository;
-
-import com.iuh.nhom05.AuthorService.entities.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-}
