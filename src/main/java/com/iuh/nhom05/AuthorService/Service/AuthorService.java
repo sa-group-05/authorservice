eae932cb781f781df965d5a1f899346fe2a15b4a
rkhang7 <khang220300@gmail.com>
2021-12-07 20:24:05 +0700

    init retry

--- src/main/java/com/iuh/nhom05/AuthorService/Service/AuthorService.java
+++ src/main/java/com/iuh/nhom05/AuthorService/Service/AuthorService.java
@@ -1,12 +1,15 @@
 package com.iuh.nhom05.AuthorService.Service;
 
 import com.iuh.nhom05.AuthorService.entities.Author;
+import io.github.resilience4j.retry.annotation.Retry;
 
 import java.util.List;
 
 public interface AuthorService {
+    @Retry(name = "basic")
     public List<Author> findAll();
     public Author findById(int id);
     public void save(Author author);
+    @Retry(name = "throwingException")
     public void deleteById(int id);
 }
