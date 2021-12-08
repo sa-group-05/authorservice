eabab1380da3e48993d507409b1bdccdde563325
Thanh Bình Huỳnh <huynhthanhbinh290@gmail.com>
2021-12-08 13:28:21 +0700

    Feat: Swagger

--- src/main/java/com/iuh/nhom05/AuthorService/AuthorServiceApplication.java
+++ src/main/java/com/iuh/nhom05/AuthorService/AuthorServiceApplication.java
@@ -2,6 +2,14 @@ package com.iuh.nhom05.AuthorService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class AuthorServiceApplication {
@@ -9,4 +17,26 @@ public class AuthorServiceApplication {
 		SpringApplication.run(AuthorServiceApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration() {
+		// Return a prepared Docket instance
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/authors/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.iuh.nhom05"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Author Service API",
+				"APIs for Author Service",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("Nhóm 05", "fit.iuh.edu.vn", "a@b.com"),
+				"API License",
+				"fit.iuh.edu.vn",
+				Collections.emptyList());
+	}
 }
--- src/main/java/com/iuh/nhom05/AuthorService/entities/Author.java
+++ src/main/java/com/iuh/nhom05/AuthorService/entities/Author.java
@@ -1,24 +1,32 @@
 package com.iuh.nhom05.AuthorService.entities;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.*;
+//import javax.persistence.*;
 
-@Entity
+//@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Table(name = "author")
+//@Table(name = "author")
+@ApiModel(description = "Details about the author")
 public class Author {
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+//    @Id
+//    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @ApiModelProperty(notes = "The unique id of the author")
     private int id;
+    @ApiModelProperty(notes = "The author's firstName")
     private String firstName;
+    @ApiModelProperty(notes = "The author's lastName")
     private String lastName;
+    @ApiModelProperty(notes = "The author's dateOfBirth")
     private String dateOfBirth;
+    @ApiModelProperty(notes = "The author's area")
     private String area;
 }
